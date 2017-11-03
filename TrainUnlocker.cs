bfb7a4a922010643dc48ba5b68139241b1628874
Phaser Array <phaserarrayksp@gmail.com>
2017-11-03 23:05:57 +0200

    Starting point for the plugin

---
+++ TrainUnlocker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+
+namespace PhaserArray.TrainUnlocker
+{
+    public class TrainUnlocker : RocketPlugin<TrainUnlockerConfiguration>
+	{
+	    public const string Version = "v1.0";
+
+	    private TrainUnlockerConfiguration _config;
+
+	    protected override void Load()
+	    {
+		    _config = Configuration.Instance;
+
+		    Logger.Log("Starting TrainUnlocker " + Version + "!");
+	    }
+
+	    protected override void Unload()
+	    {
+	    }
+	}
+}
---
+++ TrainUnlockerConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API;
+
+namespace PhaserArray.TrainUnlocker
+{
+	public class TrainUnlockerConfiguration : IRocketPluginConfiguration
+	{
+		public bool UnlockOccupied;
+
+		public void LoadDefaults()
+		{
+			UnlockOccupied = true;
+		}
+	}
+}
